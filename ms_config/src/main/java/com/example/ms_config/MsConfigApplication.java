1b6a03a64641e17378ca228ed3c6c9423eb54808
muspriandi <1.muspriandi@gmail.com>
2021-09-06 20:52:36 +0700

    edit config ports

--- ms_config/src/main/java/com/example/ms_config/MsConfigApplication.java
+++ ms_config/src/main/java/com/example/ms_config/MsConfigApplication.java
@@ -3,9 +3,11 @@ package com.example.ms_config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableConfigServer
+@RefreshScope
 public class MsConfigApplication {
 
 	public static void main(String[] args) {
