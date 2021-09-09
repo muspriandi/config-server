a827740401efa5c2b3612b8953997a355fe79579
muspriandi <1.muspriandi@gmail.com>
2021-09-09 15:33:28 +0700

    test

--- ms_config/src/main/java/com/example/ms_config/MsConfigApplication.java
+++ ms_config/src/main/java/com/example/ms_config/MsConfigApplication.java
@@ -1,35 +1,31 @@
 package com.example.ms_config;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+// import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.environment.Environment;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEncryptableProperties
+// @EnableEncryptableProperties
 @RestController
 public class MsConfigApplication {
 
-	@Value("${hi}")
+	@Value("${hi: Default}")
 	private String hello;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsConfigApplication.class, args);
 	}
 
-
 	@GetMapping("/hello")
 	public String hello() {
 		return hello;
 	}
-	
-
 
 }
