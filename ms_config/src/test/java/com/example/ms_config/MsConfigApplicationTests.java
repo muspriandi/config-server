11eabb521d6d95a6c414bdf62d14dae7073751fe
muspriandi <1.muspriandi@gmail.com>
2021-09-09 10:20:46 +0700

    first commit

---
+++ ms_config/src/test/java/com/example/ms_config/MsConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
