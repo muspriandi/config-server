29ca03ca8cbffa5fa6b6cb80950a84b0d3691406
muspriandi <1.muspriandi@gmail.com>
2021-09-13 10:50:15 +0700

    test

--- ms_client/src/main/java/com/example/ms_client/MsClientApplication.java
+++ ms_client/src/main/java/com/example/ms_client/MsClientApplication.java
@@ -1,7 +1,6 @@
 package com.example.ms_client;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -16,7 +15,7 @@ public class MsClientApplication {
 
 	@Autowired
 	private Environment env;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(MsClientApplication.class, args);
 	}
